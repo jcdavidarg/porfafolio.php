eba97a39eefab238943ed93f7d486612eb66e44a
jcdavidarg <jcdavidarg@gmail.com>
2024-04-03 20:35:52 -0300

    pagina header creada

---
+++ header.php
@@ -0,0 +1,13 @@
+<ul>
+    <li>
+        <a href="./index.php">Inicio</a>
+    </li>
+    <li>
+        <a href="./portafolio.php">Portafolio</a>
+    </li>
+    <li>
+        <a href="./logout.php">Cerrar</a>
+    </li>
+</ul>
+
+
