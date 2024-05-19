9fd96fabc2812f222d7a3549e43953d44a267dd3
jcdavidarg <jcdavidarg@gmail.com>
2024-05-19 00:44:45 -0300

    logic to insert database information

--- portafolio.php
+++ portafolio.php
@@ -2,11 +2,25 @@
 <?php include("./connection.php"); ?>
 <?php
 
+
+if ($_POST) {
+    //print_r($_POST);
+
+    $nombre = $_POST['nombre'];
+
+    $objConexion = new Connection();
+
+    //$sql = "INSERT INTO `proyectos` (`id`, `nombre`, `imagen`, `descripcion`) VALUES (NULL, 'proyecto1', 'imagen 1', 'imagen del proyecto 1');";
+    $sql = "INSERT INTO `proyectos` (`id`, `nombre`, `imagen`, `descripcion`) VALUES (NULL, '$nombre', 'imagen 1', 'imagen del proyecto 1');";
+
+    $objConexion->ejecutar($sql);
+    //$mostrar = $objConexion->ejecutar($sql);
+    //print_r($mostrar);
+}
+
 $objConexion = new Connection();
-
-$sql = "INSERT INTO `proyectos` (`id`, `nombre`, `imagen`, `descripcion`) VALUES (NULL, 'proyecto1', 'imagen 1', 'imagen del proyecto 1');";
-
-$objConexion->ejecutar($sql);
+$proyectos = $objConexion->consultar("SELECT * FROM `proyectos`");
+//print_r($proyectos);
 
 ?>
 <br />
@@ -20,7 +34,7 @@ $objConexion->ejecutar($sql);
                     Datos del proyecto
                 </div>
                 <div class="card-body">
-                    <form action="./portafolio.php" method="post">
+                    <form action="./portafolio.php" method="post" enctype="multipart/form-data">
 
                         Nombre del proyecto: <input class="form-control" type="text" name="nombre" id="">
                         <br />
@@ -39,19 +53,20 @@ $objConexion->ejecutar($sql);
                         <th>ID</th>
                         <th>Nombre</th>
                         <th>Imagen</th>
+                        <th>Descripción</th>
+                        <th>Acciones</th>
                     </tr>
                 </thead>
                 <tbody>
+                    <?php foreach($proyectos as $proyecto) {?>
                     <tr>
-                        <td>1</td>
-                        <td>Aplicacion web</td>
-                        <td>imagen.jpg</td>
+                        <td class="align-middle"><?php echo $proyecto['id']; ?></td>
+                        <td class="align-middle"><?php echo $proyecto['nombre']; ?></td>
+                        <td class="align-middle"><?php echo $proyecto['imagen']; ?></td>
+                        <td class="align-middle"><?php echo $proyecto['descripcion']; ?></td>
+                        <td class="align-middle"> <a class="btn btn-danger" href="#">Eliminar</a> </td>
                     </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Aplicacion de escritorio</td>
-                        <td>imagen2.jpg</td>
-                    </tr>
+                    <?php } ?>
                 </tbody>
             </table>
         </div>
